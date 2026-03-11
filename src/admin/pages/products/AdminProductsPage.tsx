408b3ea7fbb307249a337eff12eed00722b6104e
PacoAlexM <pako_ale_martell@hotmail.com>
2026-03-10 23:38:05 -0600

    configuracion de paginas de productos de admin

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -10,12 +10,13 @@ export const AdminProductsPage = () => {
         <>
             <div className="flex justify-between items-center">
                 <AdminTitle title="Productos" subTitle="Aquí puedes ver y administrar tus productos." />
-
-                <Link to="/admin/products/new">
+                <div className="flex justify-end mb-10 gap-4">
                     <Button>
-                        <PlusIcon /> Nuevo Producto
+                        <Link to="/admin/products/new">
+                                <PlusIcon /> Nuevo Producto
+                        </Link>
                     </Button>
-                </Link>
+                </div>
             </div>
 
             <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
